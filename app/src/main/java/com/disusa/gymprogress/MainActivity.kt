efd014d27c25a41ade41851a996330b5153e34cb
FaresDisusa <fares.disusa@gmail.com>
2023-11-25 23:40:34 +0100

    Initial commit

---
+++ app/src/main/java/com/disusa/gymprogress/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.disusa.gymprogress
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.disusa.gymprogress.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_fragment,HomeFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/disusa/gymprogress/fragments/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.disusa.gymprogress.fragments
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.disusa.gymprogress.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
+}
