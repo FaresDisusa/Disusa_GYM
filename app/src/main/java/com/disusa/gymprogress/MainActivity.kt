87f36792da32483244ede5eecbb8f8759ed84658
FaresDisusa <fares.disusa@gmail.com>
2023-12-23 00:14:07 +0100

    1.04

--- app/src/main/java/com/disusa/gymprogress/MainActivity.kt
+++ app/src/main/java/com/disusa/gymprogress/MainActivity.kt
@@ -2,6 +2,8 @@ package com.disusa.gymprogress
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.disusa.gymprogress.fragments.HomeFragment
 
 
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_fragment, HomeFragment())
         transaction.addToBackStack(null)
