9bac5dbb9dbc4af333e1f4666e22373d10e89e9b
FaresDisusa <fares.disusa@gmail.com>
2023-12-31 23:57:42 +0100

    1.08

--- app/src/main/java/com/disusa/gymprogress/MainActivity.kt
+++ app/src/main/java/com/disusa/gymprogress/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.disusa.gymprogress.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +22,56 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.main_fragment, HomeFragment())
         transaction.addToBackStack(null)
         transaction.commit()
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.nav_footer)
+        bottomNavigation.itemIconTintList = null
+        bottomNavigation.itemTextColor = null
+        bottomNavigation.itemRippleColor = null
+        bottomNavigation.stateListAnimator = null
+        bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    menuItem.setIcon(R.drawable.home_degrade)
+                    bottomNavigation.menu.findItem(R.id.stat)?.setIcon(R.drawable.progress)
+                    bottomNavigation.menu.findItem(R.id.calender)?.setIcon(R.drawable.calender_white)
+                    bottomNavigation.menu.findItem(R.id.account)?.setIcon(R.drawable.account)
+                    bottomNavigation.menu.findItem(R.id.settings)?.setIcon(R.drawable.settings)
+                    true
+                }
+                R.id.stat -> {
+                    menuItem.setIcon(R.drawable.progress_degrade)
+                    bottomNavigation.menu.findItem(R.id.home)?.setIcon(R.drawable.home_white)
+                    bottomNavigation.menu.findItem(R.id.calender)?.setIcon(R.drawable.calender_white)
+                    bottomNavigation.menu.findItem(R.id.account)?.setIcon(R.drawable.account)
+                    bottomNavigation.menu.findItem(R.id.settings)?.setIcon(R.drawable.settings)
+                    true
+                }
+                R.id.calender -> {
+                    menuItem.setIcon(R.drawable.calender_degrade)
+                    bottomNavigation.menu.findItem(R.id.stat)?.setIcon(R.drawable.progress)
+                    bottomNavigation.menu.findItem(R.id.home)?.setIcon(R.drawable.home_white)
+                    bottomNavigation.menu.findItem(R.id.account)?.setIcon(R.drawable.account)
+                    bottomNavigation.menu.findItem(R.id.settings)?.setIcon(R.drawable.settings)
+                    true
+                }
+                R.id.account -> {
+                    menuItem.setIcon(R.drawable.account_degrade)
+                    bottomNavigation.menu.findItem(R.id.stat)?.setIcon(R.drawable.progress)
+                    bottomNavigation.menu.findItem(R.id.calender)?.setIcon(R.drawable.calender_white)
+                    bottomNavigation.menu.findItem(R.id.home)?.setIcon(R.drawable.home_white)
+                    bottomNavigation.menu.findItem(R.id.settings)?.setIcon(R.drawable.settings)
+                    true
+                }
+                R.id.settings -> {
+                    menuItem.setIcon(R.drawable.settings_degrade)
+                    bottomNavigation.menu.findItem(R.id.stat)?.setIcon(R.drawable.progress)
+                    bottomNavigation.menu.findItem(R.id.calender)?.setIcon(R.drawable.calender_white)
+                    bottomNavigation.menu.findItem(R.id.account)?.setIcon(R.drawable.account)
+                    bottomNavigation.menu.findItem(R.id.home)?.setIcon(R.drawable.home_white)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
